fdad24284aeb961435a0bba13aebbd380a18bdde
khushboo29 <ktiwari1991@gmail.com>
2019-03-30 16:25:13 +0530

    practical example of decorators

--- practical_example.py
+++ practical_example.py
@@ -1,10 +1,12 @@
 #practical example of decorators
 #logging to keep track how many time a specific function run with passed arguments
+from functools import wraps
 
 def my_logger(original_function):
     import logging
     logging.basicConfig(filename='{}.log'.format(original_function.__name__), level=logging.INFO)
 
+    @wraps(original_function)
     def wrapper(*args,**kwargs):
         logging.info('Ran with args: {} and kwargs: {}'.format(args,kwargs))
         return original_function(*args,**kwargs)
@@ -13,6 +15,7 @@ def my_logger(original_function):
 def my_timer(original_function):
     import time
 
+    @wraps(original_function)
     def wrapper(*args,**kwargs):
         t1 = time.time()
         result = original_function(*args,**kwargs)
@@ -27,8 +30,15 @@ def display():
     print("display function run")
 '''
 
+import time
+
 @my_logger
+@my_timer
 def display_info(name,age):
+    time.sleep(1)
     print("name is: {} and age is: {}".format(name,age))
 
-display_info("Corey",45)
+display_info = my_logger(my_timer(display_info))
+                                                   
+display_info("Tom",22)
+
